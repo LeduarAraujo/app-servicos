63bbd0990c68dcc066322d31e69e5e90c01700fc
Leduar Araujo <LeduarAraujo>
2019-09-01 00:12:30 -0300

    Ajuste do corpo e criação das rotas

--- src/app/template/temp-nav/temp-nav.component.ts
+++ src/app/template/temp-nav/temp-nav.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+// Rotas
+import { Rotas } from '../../utils/utils';
+
 @Component({
   selector: 'app-temp-nav',
   templateUrl: './temp-nav.component.html',
   styleUrls: ['./temp-nav.component.css']
 })
 export class TempNavComponent implements OnInit {
+
+  titulo = "Projeto Servico";
+  rota = Rotas;
 
   constructor() { }
 
