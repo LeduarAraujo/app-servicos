433a8475928175376a0c48a630ec4a535448d39b
Leduar Araujo <LeduarAraujo>
2019-08-31 21:32:53 -0300

    Adição dos comentarios.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+// Templates 
 import { TempNavComponent } from './template/temp-nav/temp-nav.component';
 import { TempRodComponent } from './template/temp-rod/temp-rod.component';
+
+// Componentes
 import { AppHomeComponent } from './components/app-home/app-home.component';
 
 @NgModule({
