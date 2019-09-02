29a7521a7af805815c82db8febea4a31746b1a4b
Paula <leduar.araujo.paula@accenture.com>
2019-09-02 10:25:21 -0300

    Inclusão elemento loading na tela de cadastro. Resta concluir a ação.

--- src/app/components/app-cadastro/app-cadastro.component.ts
+++ src/app/components/app-cadastro/app-cadastro.component.ts
@@ -12,6 +12,8 @@ export class AppCadastroComponent implements OnInit {
   colecaoEstados;
   colecaoCidades;
 
+  displayNone;
+
   constructor() { }
 
   ngOnInit() {
@@ -23,13 +25,16 @@ export class AppCadastroComponent implements OnInit {
     .then(response => response.json()).then(jsonArray => {
       this.colecaoEstados = jsonArray;
     });
+    this.displayNone = 'none';
   }
 
   getMunicipios(inputState) {
+    this.displayNone = '';
     let codigoUF = inputState.split("-");
     fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados/" + codigoUF[0] + "/municipios")
     .then(response => response.json()).then(jsonArray => {
       this.colecaoCidades = jsonArray;
     });
+    this.displayNone = 'none';
   }
 }
